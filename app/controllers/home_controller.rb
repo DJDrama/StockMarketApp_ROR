886a5e1cb7d7df9963d6d7507575bb6572d64235
DJDrama <putzzang@naver.com>
2021-04-30 00:04:45 +0900

    Error Handling2 exception handling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,14 @@
 class HomeController < ApplicationController
   def index
     @api = StockQuote::Stock.new(api_key: Rails.application.credentials.stock_key)
-    if params[:ticker]
-      @stock = StockQuote::Stock.quote(params[:ticker])
+    if params[:ticker] == ""
+      @nothing = "Hey! You Forgot To Enter A Symbol!"
+    elsif params[:ticker]
+      begin
+        @stock = StockQuote::Stock.quote(params[:ticker])
+      rescue
+        @error = "Hey! That Stock Symbol Doesn't Exist. Please Try Again!"
+      end
     end
   end
 
