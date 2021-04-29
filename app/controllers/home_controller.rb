3d8f49c4cd11cdd114893cb078ff70a155ab5019
DJDrama <putzzang@naver.com>
2021-04-29 23:52:13 +0900

    error handling1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @api = StockQuote::Stock.new(api_key: '')
+    @api = StockQuote::Stock.new(api_key: Rails.application.credentials.stock_key)
+    if params[:ticker]
+      @stock = StockQuote::Stock.quote(params[:ticker])
+    end
   end
 
   def about
